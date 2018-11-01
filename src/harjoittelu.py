891f4f746fc5b272205faf93ca2ced34c1232f17
Niska Päivö A V <paivo@da5-cs-b221-08.pc.helsinki.fi>
2018-11-01 16:07:01 +0200

    lisattiin harjoittelu

---
+++ src/harjoittelu.py
@@ -0,0 +1,27 @@
+kokonaisluku = 42
+print(kokonaisluku)
+print(type(kokonaisluku))
+
+liukuluku = 3.14159
+print(liukuluku)
+print(type(liukuluku))
+
+merkkijono = "42/3.14159 ~ 13.37"
+print(merkkijono)
+print(type(merkkijono))
+
+muuttumatonlista = (42, 21, 10.5)
+print(muuttumatonlista)
+print(type(muuttumatonlista))
+
+lista = [42, 21, 10.5]
+print(lista)
+print(type(lista))
+
+joukko = {"123", "456", 42}
+print(joukko)
+print(type(joukko))
+
+sanakirja = {"eka":1, "toka": 2}
+print(sanakirja)
+print(type(sanakirja))
