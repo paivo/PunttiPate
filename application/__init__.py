2c90a0b8b56172a23859a1b673cfc36bbd5dca13
Pave <paveee777@gmail.com>
2018-11-15 23:28:38 +0200

    viikko 3, 4. kohtaan saakka ennen postgre käyttöönottoa

--- application/__init__.py
+++ application/__init__.py
@@ -1,24 +1,38 @@
-# Tuodaan Flask käyttöön
+# flask-sovellus
 from flask import Flask
 app = Flask(__name__)
 
-# Tuodaan SQLAlchemy käyttöön
+# tietokanta
 from flask_sqlalchemy import SQLAlchemy
-# Käytetään tasks.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
-# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
-# samassa paikassa
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
-# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
 app.config["SQLALCHEMY_ECHO"] = True
 
-# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
 db = SQLAlchemy(app)
 
-# Luetaan kansiosta application tiedoston views sisältö
+# oman sovelluksen toiminnallisuudet
 from application import views
 
 from application.tasks import models
 from application.tasks import views
 
-# Luodaan lopulta tarvittavat tietokantataulut
+from application.auth import models
+from application.auth import views
+
+# kirjautuminen
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# luodaan taulut tietokantaan tarvittaessa
 db.create_all()
