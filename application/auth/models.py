611adadbd7221bd255e42089d8dc81aea3bed186
Niska Päivö A V <paivo@dx5-cs-c221-04.pc.helsinki.fi>
2018-11-23 15:33:33 +0200

    lisättiin hipsut taskin models.py tiedostoon

--- application/auth/models.py
+++ application/auth/models.py
@@ -33,7 +33,7 @@ class User(Base):
 
 
     @staticmethod
-    def find_users_with_no_tasks(done=0):
+    def find_users_with_no_tasks(done='1'):
         stmt = text("SELECT Account.id, Account.name FROM Account"
                      " LEFT JOIN Task ON Task.account_id = Account.id"
                      " WHERE (Task.done IS null OR Task.done = :done)"
